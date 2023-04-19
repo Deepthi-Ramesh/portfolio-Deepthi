40af2aa51eb73b8f434589c4e44ccebb9c5159e5
premcloudville <sihtechhunter@gmail.com>
2023-04-19 21:44:29 +0530

    mail updated

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -1,29 +1,45 @@
-import React from "react";
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 import "./contact.css";
 import Appcomponent from "./Appcomponent";
 function Contact(){
+    const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm(
+        'service_9nutsld',
+         'template_0cv60ye',
+          form.current, 
+          'xhH4yfrE9ey0IPJun')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
     return(
         <div className="contact_con">
             <div className="contact_head">CONTACT ME!</div>
             <div className="contact_items">
               <div className="contact_para">
               I’m interested in freelance opportunities – especially ambitious or large projects . If you really impressed with skills , please contact me.
-
-
               </div>
+              <form className='wrap-form' ref={form} onSubmit={sendEmail}>
                 <div className="form">
-                    <input type="text" placeholder="Name"/>
+                    <input type="text" placeholder="Name" name="user_name"/>
                 </div>
                 <div className="form">
-                <input type="email" placeholder="Email"/>
+                <input type="email" placeholder="Email" name="user_email"/>
                 </div>
                 <div className="form2">
-                    <textarea type="text" placeholder="Message" id="message" ></textarea>
+                    <textarea type="text" placeholder="Message" id="message" name="message" ></textarea>
                 </div>
                 <div>
-                <button className="contact_btn">Send Me</button>
+                <button className="contact_btn" type="submit">Send Me</button>
                 </div>
-                
+                </form>
             </div>
            
 </div>
